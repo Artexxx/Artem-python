b54e9d0eac268c3847789ed09b1ae7dfc405aba8
Artexxx <54672403+Artexxx@users.noreply.github.com>
2020-01-31 19:23:24 +0300

    Update рекурсия.py
    более подробно в простую рекурсию

--- учим_алгоритмы/рекурсия.py
+++ учим_алгоритмы/рекурсия.py
@@ -1,5 +1,32 @@
-def sum(list = [1,2,3]):
-    if list==[]:
-        return 0
-    return 1 + sum(list)
-sum([1,4,5])
+def factorial(num):
+    if num == 0:  # Базовый  случай
+        return 1
+    return factorial(num - 1) * num
+
+
+# Сумма цифр числа
+def sum_n(a):
+    if a:
+        return a % 10 + sum_n(a // 10)
+    # Базовый  случай
+    return 0
+
+
+# Цифры числа справа налево
+def reverse_n(n):
+    # Базовый  случай
+    if (n < 10):
+        return n
+    else:
+        print(n % 10)
+    return reverse_n(n // 10)
+
+
+# Цифры числа слева направо
+def lToR(n):
+    # Базовый  случай
+    if n == 0:
+        return
+    else:
+        lToR(n // 10)
+        print(n % 10, end=' ')
