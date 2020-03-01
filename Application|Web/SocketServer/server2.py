4223024d4dff967c7aaa1830fac0e76bbb6abab6
Artexxx <tyktyk2004@gmail.com>
2020-03-01 19:35:54 +0300

    add server2.py

---
+++ Application|Web/SocketServer/server2.py
@@ -0,0 +1,25 @@
+# tcp сервер запускается на IP адресе 0.0.0.0 и TCP порту 2222
+# обслуживаеь 10 клиентов одновременно
+from threading import Thread
+import socket
+
+HOST = '0.0.0.0'
+PORT = 2222
+
+
+def server(conn, addr):
+    while True:
+        data = conn.recv(1024)
+        if data.decode() == 'close':
+            break
+        conn.send(data)
+    conn.close()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(10)
+    for _ in range(10):
+        conn, addr = s.accept()
+        t = Thread(target=server, args=(conn, addr))
+        t.start()
