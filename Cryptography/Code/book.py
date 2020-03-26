4766ba71dfdce0c9385a94053551f5c3f431298b
Arem <tyktyk2004@gmail.com>
2020-03-26 20:46:20 +0300

    add shifr

---
+++ Cryptography/Code/book.py
@@ -0,0 +1,42 @@
+""" Книжный шифр --  один из самых криптостойких методов шифрования.
+    Особенность шифра заключена в ключе-книге, которая может являться любой
+        текстовой информацией."""
+from random import choice
+from re import findall
+
+cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
+if cryptMode not in ['E', 'D']:
+    print("Error: mode is not Found!");
+    raise SystemExit
+startMessage = input("Write the message: ")
+bookKey = input("Write the name file book-key: ")
+
+
+def regular(text):
+    template = r"[0-9]+"
+    return findall(template, text)
+
+
+def encryptDecrypt(mode, message, key, final=""):
+    with open(key) as bookKey:
+        book = bookKey.read()
+    if mode == 'E':
+        for symbolMessage in message:
+            listIndexKey = []
+            for indexKey, symbolKey in enumerate(book):
+                if symbolMessage == symbolKey:
+                    listIndexKey.append(indexKey)
+            try:
+                final += str(choice(listIndexKey)) + '/'
+            except IndexError:
+                pass
+    else:
+        for numbers in regular(message):
+            for indexKey, symbolKey in enumerate(book):
+                if numbers == str(indexKey):
+                    final += symbolKey
+    return final
+
+
+print("Final message:", encryptDecrypt(cryptMode, startMessage, bookKey))
+
---
+++ Cryptography/Code/vishener.py
@@ -0,0 +1,24 @@
+"""Шифр Виженера -- самый знаменитый многоалфавитный шифр.
+Построен на конструкции шифра Цезаря. Представлен в виде квадрата и содержит
+в себе фактически 26 шифров Цезаря с разными смещениями по алфавиту"""
+
+cryptMode = input("[E]ncrypt|[D]ecrypt: ").upper()
+if cryptMode not in ['E', 'D']:
+    print("Error: mode is not Found!")
+    raise SystemExit
+startMessage = input("Write the message: ").upper()
+oneKey = input("Write the key: ").upper()
+
+
+def encryptDecrypt(mode, message, key, final=""):
+    key *= len(message) // len(key) + 1
+    for index, symbol in enumerate(message):
+        if mode == 'E':
+            temp = ord(symbol) + ord(key[index])
+        else:
+            temp = ord(symbol) - ord(key[index])
+        final += chr(temp % 26 + ord('A'))
+    return final
+
+
+print("Final message:", encryptDecrypt(cryptMode, startMessage, oneKey))
