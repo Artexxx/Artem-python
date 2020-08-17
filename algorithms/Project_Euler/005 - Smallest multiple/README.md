6b68fc533d81c7b3c1daca06b323f8ccd4ffdc56
artexxx <tyktyk2004@gmail.com>
2020-08-17 14:59:42 +0300

    add solution #005 ~> project euler

--- algorithms/Project_Euler/005 - Smallest multiple/README.md
+++ algorithms/Project_Euler/005 - Smallest multiple/README.md
@@ -78,6 +78,87 @@ def solution(end):
 
 ## Нормальное решение (2)
 
+- В основе лежит *фундаментальная арифметическая теорема* 
+
+1. Находим простые чисела, образующие х, где x - это число от 2 до N
+2. Разделяем подряд идущие простые числа на частоты
+    <br> Пример: [2, 2, 3, 3, 2, 3] ~>  [2, 2], [3, 2], [2, 1], [3, 1]
+3. Для каждого числа нажодим максимальную частоту
+    <br> Пример: [2, 2],  [3, 2],  [2, 1],  [3, 1] ~>  { 2: 2, 3: 2 }
+
+``` python
+import math
+
+def primeFactors(x) -> list:
+    """ Возращает простые чисела, образующие х
+
+    >>> primeFactors(24)
+    [2, 2, 2, 3]
+    """
+    if x <= 1: return []
+    for i in range(2, x + 1):
+        if x % i == 0:
+            return [i, *primeFactors(x // i)]
+
+
+def primesFrequency(primes: list) -> list:
+    """Возращает частоты последовательных простых чисел
+
+    >>> primesFrequency([2, 2, 3, 3, 2, 3])
+    [[2, 2], [3, 2], [2, 1], [3, 1]]
+    """
+    frequency = [[primes[0], 1]]
+
+    for i in range(1, len(primes)):
+        if primes[i] != primes[i - 1]:
+            frequency.append([primes[i], 1])
+        else:
+            frequency[-1][1] += 1
+    return frequency
+
+
+def primesGreatestFrequency(primesFrequencyList: list) -> dict:
+    """Возращает самые большие частоты последовательных простых чисел
+
+    >>> primesGreatestFrequency(  [2, 2],  [3, 2],  [2, 1],  [3, 1])
+    { 2: 2, 3: 2 }
+    """
+    max_frequency = {}
+    for frequency in primesFrequencyList:
+        value = max_frequency.get(frequency[0])
+        if value:
+            max_frequency[frequency[0]] = max(value, frequency[1])
+        else:
+            max_frequency[frequency[0]] = frequency[1]
+    return max_frequency
+
+
+def solution(n):
+    primes = []
+    for i in range(n, 1, -1):
+        primes.extend(primeFactors(i))
+    primesFrequencyList = primesFrequency(primes)
+    max_frequency = primesGreatestFrequency(primesFrequencyList)
+
+    result_number = 1
+    for item in max_frequency.items():
+        result_number *= math.pow(*item)
+    return result_number
+```
+```text
+  №     Время  Замедление      Число                                   Результат
+---  --------  ------------  -------  ------------------------------------------
+  1  1.04e-05  0.001%             10                                        2520
+  2  2.04e-05  0.00%              15                                      240240
+  3  2.37e-05  0.00%              20                                   232792560
+  4  2.83e-05  0.00%              25                                  7138971840
+  5  3.16e-05  0.00%              50                       354176514770971052160
+  6  3.13e-05  0.00%             100  793262935946950851294251337219553320468480
+```
+
+
+## Нормальное решение (3)
+
 Наименьшее число n, которое равномерно делится (без остатка) на каждое число в множестве {k1, k2,..., к_m},
 также известено как наименьшее общее кратное (НОК) из множества чисел.
  
--- algorithms/Project_Euler/005 - Smallest multiple/sol2.py
+++ algorithms/Project_Euler/005 - Smallest multiple/sol2.py
@@ -3,25 +3,61 @@
 
 Какое самое маленькое число делится нацело на все числа от 1 до N?
 
-
-  №     Время  Замедление      Число                            Результат
----  --------  ------------  -------  -----------------------------------
-  1  7.2e-06   0.001%             10                                 2520
-  2  7.2e-06   0.00%              15                               360360
-  3  9.9e-06   0.00%              20                            232792560
-  4  1.2e-05   0.00%              25                          26771144400
-  5  1.94e-05  0.00%              40                     5342931457063200
-  6  4.28e-05  0.00%              80  32433859254793982911622772305630400
+  №     Время  Замедление      Число                                   Результат
+---  --------  ------------  -------  ------------------------------------------
+  1  1.04e-05  0.001%             10                                        2520
+  2  2.04e-05  0.00%              15                                      240240
+  3  2.37e-05  0.00%              20                                   232792560
+  4  2.83e-05  0.00%              25                                  7138971840
+  5  3.16e-05  0.00%              50                       354176514770971052160
+  6  3.13e-05  0.00%             100  793262935946950851294251337219553320468480
 """
 
-def gcd(x, y):
-    """ Euclidean GCD Algorithm """
-    return x if y == 0 else gcd(y, x % y)
+import math
 
 
-def lcm(x, y):
-    """ Using the property lcm*gcd of two numbers = product of them """
-    return (x * y) // gcd(x, y)
+def primeFactors(x) -> list:
+    """ Возращает простые чисела, образующие х
+
+    >>> primeFactors(24)
+    [2, 2, 2, 3]
+    """
+    if x <= 1: return []
+    for i in range(2, x + 1):
+        if x % i == 0:
+            return [i, *primeFactors(x // i)]
+
+
+def primesFrequency(primes: list) -> list:
+    """"Возращает частоты последовательных простых чисел
+
+    >>> primesFrequency([2, 2, 3, 3, 2, 3])
+    [[2, 2], [3, 2], [2, 1], [3, 1]]
+    """
+    frequency = [[primes[0], 1]]
+
+    for i in range(1, len(primes)):
+        if primes[i] != primes[i - 1]:
+            frequency.append([primes[i], 1])
+        else:
+            frequency[-1][1] += 1
+    return frequency
+
+
+def primesGreatestFrequency(primesFrequencyList: list) -> dict:
+    """Возращает самые большие частоты последовательных простых чисел
+
+    >>> primesGreatestFrequency(  [2, 2],  [3, 2],  [2, 1],  [3, 1])
+    { 2: 2, 3: 2 }
+    """
+    max_frequency = {}
+    for frequency in primesFrequencyList:
+        value = max_frequency.get(frequency[0])
+        if value:
+            max_frequency[frequency[0]] = max(value, frequency[1])
+        else:
+            max_frequency[frequency[0]] = frequency[1]
+    return max_frequency
 
 
 def solution(n):
@@ -36,16 +72,21 @@ def solution(n):
     >>> solution(22)
     232792560
     """
-    g = 1
-    for i in range(1, n + 1):
-        g = lcm(g, i)
-    return g
+    primes = []
+    for i in range(n, 1, -1):
+        primes.extend(primeFactors(i))
+    primesFrequencyList = primesFrequency(primes)
+    max_frequency = primesGreatestFrequency(primesFrequencyList)
+
+    result_number = 1
+    for item in max_frequency.items():
+        result_number *= math.pow(*item)
+    return result_number
 
 
-
-if __name__ == "__main__":
-    print(solution(int(input().strip())))
+if __name__ == '__main__':
+    print(solution(20))
     # ### Run Time-Profile Table ###
-    # import sys; sys.path.append('..')
+    # import sys;sys.path.append('..')
     # from time_profile import my_time_this
-    # my_time_this(solution, [10, 15, 20, 25, 40, 80])
+    # my_time_this(solution, [10, 15, 20, 25, 50, 100])
