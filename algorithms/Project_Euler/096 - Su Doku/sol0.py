420e728634cd40f2b4f3e7824b6412a9b4cd1e7d
Artem <54672403+Artexxx@users.noreply.github.com>
2020-09-13 22:05:57 +0300

    add solution ~> project euler

--- algorithms/Project_Euler/096 - Su Doku/sol0.py
+++ algorithms/Project_Euler/096 - Su Doku/sol0.py
@@ -1,12 +1,9 @@
 from dlxsudoku.sudoku import Sudoku
 
-from timeit import default_timer as timer
 s = [l for l in open('p096_sudoku.txt').readlines() if l[0].isdigit()]
 sudokus = [Sudoku(''.join(s[i * 9:(i + 1) * 9])) for i in range(50)]
-t = timer()
 for sudoku in sudokus:
     sudoku.solve()
 
 result = sum(int(sudoku.to_oneliner()[:3]) for sudoku in sudokus)
-print(timer()-t)
 print(result)
