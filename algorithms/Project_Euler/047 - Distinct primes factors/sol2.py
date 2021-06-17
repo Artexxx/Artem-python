6b27e83e43db132da172073c324193399323b550
Artem <54672403+Artexxx@users.noreply.github.com>
2021-06-17 20:29:53 +0300

    add solution ~> project euler

--- algorithms/Project_Euler/047 - Distinct primes factors/sol2.py
+++ algorithms/Project_Euler/047 - Distinct primes factors/sol2.py
@@ -13,62 +13,32 @@
 Найдите первые четыре последовательных числа, каждое из которых имеет четыре отличных друг от друга простых множителя. Каким будет первое число?
 
   Время  Замедление    Аргумент      Результат
--------  ------------  ----------  ----------- <913330 function calls>
-1.33932  133.932%               4       134043 (Ответ)
+-------  ------------  ----------  ----------- <12501 function calls>
+  0.193  19,332%               4       134043 (Ответ)
 """
 
-
-def memoize(f):
-    cache = {}
-
-    def wrapper(*args):
-        if not args in cache:
-            cache[args] = f(*args)
-            # print(f"[#] F(",int(*args), ')\t => \t', dict(cache[args])) # TEST OUTPUT
-        return cache[args]
-
-    return wrapper
-
-@memoize
-def prime_factors(n) -> set:
-    """Return a set of n's prime factors
-
-    >>> prime_factors(220)
-    [2, 5, 11]
-    """
-    i = 2
-    factors = set()
-    while i * i <= n:
-        if n % i != 0:
-            i += 1
-        else:
-            n //= i
-            factors.add(i)
-    if n > 1:
-        factors.add(n)
-    return factors
-
-
-def solution(start=1):
+def solution(LIMIT=10**6):
     """Находит первое из первых четырех последовательных чисел, каждое из которых имеет четыре отличных друг от друга простых множителя.
     """
-    number = start
-    while True:
-        if (len(prime_factors(number)) == 4 and
-            len(prime_factors(number + 1)) == 4 and
-            len(prime_factors(number + 2)) == 4 and
-            len(prime_factors(number + 3)) == 4):
-            break
-        number += 1
-    return number
+    sieve = [0] * (LIMIT+1)
+    len_sequence = 0
 
+    for n in range(2, LIMIT+1):
+        if sieve[n] == 0:  # n is prime
+            sieve[n::n] = [x + 1 for x in sieve[n::n]]
+        if sieve[n] == 4:
+            len_sequence += 1
+            if len_sequence == 4:
+                return n - 3  # нужно первое число
+        else:
+            len_sequence = 0
 
 if __name__ == '__main__':
+
     ### Run Time-Profile Table ###
     import sys; sys.path.append('..')
     from time_profile import TimeProfile
     import cProfile
     with cProfile.Profile() as pr:
-        TimeProfile(solution, [1, 134043+1, 238203+1])
+        TimeProfile(solution)
     pr.print_stats()
-
